2e4adc0c83e440d150d35ace87893140e859c14a
frans <afriandifrans@gmail.com>
2023-01-19 10:13:02 +0700

    update

---
+++ pkg/mysql/mysql.go
@@ -0,0 +1,24 @@
+package mysql
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+// Connetion DataBases
+func DatabaseInit() {
+	var err error
+	// dsn := "root:@tcp(localhost:3306)/dumbmerch?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:6cC6l4FXRLJfU8GWUFRB@tcp(containers-us-west-154.railway.app:5647)/railway?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Conneted To Databases")
+}
